ae602df7a53dfbd0b3e4e54953d1a9ab21d178b8
lunarwhite <dwcn22@outlook.com>
2022-02-06 19:39:01 +0800

    feat: implement "http.Handler" interface

--- main.go
+++ main.go
@@ -6,20 +6,23 @@ import (
 	"net/http"
 )
 
-// handler echoes r.URL.Path
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-}
+// Engine is the uni handler for all requests
+type Engine struct{}
 
-// handler echoes r.URL.Header
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
