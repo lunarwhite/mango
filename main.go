61f317abdc7da0869956924e302af6c7349a4d25
lunarwhite <dwcn22@outlook.com>
2022-02-06 23:40:06 +0800

    test: run demo for router matching

--- main.go
+++ main.go
@@ -11,16 +11,19 @@ func main() {
 	r.GET("/", func(c *mango.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello mango</h1>")
 	})
+
 	r.GET("/hello", func(c *mango.Context) {
 		// expect /hello?name=mangouser
-		c.String(http.StatusOK, "hello %s, you are at %s\n", c.Query("name"), c.Path)
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *mango.Context) {
-		c.JSON(http.StatusOK, mango.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *mango.Context) {
+		// expect /hello/mangouser
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *mango.Context) {
+		c.JSON(http.StatusOK, mango.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
