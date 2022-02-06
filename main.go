32e4c51eaae104d3bb0971c9bf4d1a57faa29141
lunarwhite <dwcn22@outlook.com>
2022-02-07 00:37:00 +0800

    test: run demo for router group

--- main.go
+++ main.go
@@ -8,23 +8,33 @@ import (
 
 func main() {
 	r := mango.New()
-	r.GET("/", func(c *mango.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello mango</h1>")
+	r.GET("/index", func(c *mango.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *mango.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello mango</h1>")
+		})
 
-	r.GET("/hello", func(c *mango.Context) {
-		// expect /hello?name=mangouser
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.GET("/hello/:name", func(c *mango.Context) {
-		// expect /hello/mangouser
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *mango.Context) {
-		c.JSON(http.StatusOK, mango.H{"filepath": c.Param("filepath")})
-	})
+		v1.GET("/hello", func(c *mango.Context) {
+			// expect /hello?name=mangouser
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *mango.Context) {
+			// expect /hello/mangouser
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *mango.Context) {
+			c.JSON(http.StatusOK, mango.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
