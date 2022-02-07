ef4ad57d902484436bd2a7bfeb6b31135e7da9af
lunarwhite <dwcn22@outlook.com>
2022-02-07 10:45:16 +0800

    test: run demo for middleware

--- main.go
+++ main.go
@@ -1,38 +1,37 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"mango"
 )
 
+func onlyForV2() mango.HandlerFunc {
+	return func(c *mango.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
 func main() {
 	r := mango.New()
-	r.GET("/index", func(c *mango.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Use(mango.Logger()) // global midlleware
+	r.GET("/", func(c *mango.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello mango</h1>")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *mango.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello mango</h1>")
-		})
 
-		v1.GET("/hello", func(c *mango.Context) {
-			// expect /hello?name=mangouser
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *mango.Context) {
 			// expect /hello/mangouser
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-		v2.POST("/login", func(c *mango.Context) {
-			c.JSON(http.StatusOK, mango.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
 		})
 	}
 
