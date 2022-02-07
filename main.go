666beffa5e4533c2943bf5e6f2d2c8e2f0a34be0
lunarwhite <dwcn22@outlook.com>
2022-02-07 14:43:00 +0800

    test: run demo for panic handling

--- main.go
+++ main.go
@@ -1,44 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
-	"time"
 
 	"mango"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := mango.New()
-	r.Use(mango.Logger())
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{Name: "User", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
-
+	r := mango.Default()
 	r.GET("/", func(c *mango.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
+		c.String(http.StatusOK, "Hello mangouser\n")
 	})
-	r.GET("/students", func(c *mango.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", mango.H{
-			"title":  "mango",
-			"stuArr": [2]*student{stu1, stu2},
-		})
+
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *mango.Context) {
+		names := []string{"mangouser"}
+		c.String(http.StatusOK, names[100])
 	})
 
 	r.Run(":9999")
