86286a0cca7cfd582bdc0f6eddd70d8f9d6c9b5d
lunarwhite <dwcn22@outlook.com>
2022-02-07 12:21:34 +0800

    test: run demo for template

--- main.go
+++ main.go
@@ -1,39 +1,45 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"html/template"
 	"net/http"
 	"time"
 
 	"mango"
 )
 
-func onlyForV2() mango.HandlerFunc {
-	return func(c *mango.Context) {
-		// Start timer
-		t := time.Now()
-		// if a server error occurred
-		c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
-		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
-	}
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
 	r := mango.New()
-	r.Use(mango.Logger()) // global midlleware
-	r.GET("/", func(c *mango.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello mango</h1>")
+	r.Use(mango.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
 	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
 
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *mango.Context) {
-			// expect /hello/mangouser
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	stu1 := &student{Name: "User", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+
+	r.GET("/", func(c *mango.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *mango.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", mango.H{
+			"title":  "mango",
+			"stuArr": [2]*student{stu1, stu2},
 		})
-	}
+	})
 
 	r.Run(":9999")
 }
